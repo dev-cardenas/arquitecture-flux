3b47b817469723a2add3d2e255afd77feb0ac366
dev-cardenas <dev.cardenas@gamil.com>
2019-09-30 21:53:59 -0400

    get data api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,107 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Campiones muy buenos</strong>
-        <span>USD 80,12</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>Agregar al Carrito</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Campiones muy buenos</strong>
-        <span>USD 80,12</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
+    this.setState({ products: data });
+  }
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-          <span>Agregar al Carrito</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Campiones muy buenos</strong>
-        <span>USD 80,12</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />
+              </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-
-          <span>Agregar al Carrito</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Campiones muy buenos</strong>
-        <span>USD 80,12</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-
-          <span>Agregar al Carrito</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Campiones muy buenos</strong>
-        <span>USD 80,12</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-
-          <span>Agregar al Carrito</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Campiones muy buenos</strong>
-        <span>USD 80,12</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-
-          <span>Agregar al Carrito</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>Agregar al Carrito</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
