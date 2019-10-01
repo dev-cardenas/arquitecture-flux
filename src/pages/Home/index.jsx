3e64295516fde9d46ccb181629aebdcefe75c0c7
dev-cardenas <dev.cardenas@gamil.com>
2019-09-30 20:45:27 -0400

    styled in home and add polished

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Campiones muy buenos</strong>
+        <span>USD 80,12</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>Agregar al Carrito</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Campiones muy buenos</strong>
+        <span>USD 80,12</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>Agregar al Carrito</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Campiones muy buenos</strong>
+        <span>USD 80,12</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>Agregar al Carrito</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Campiones muy buenos</strong>
+        <span>USD 80,12</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>Agregar al Carrito</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Campiones muy buenos</strong>
+        <span>USD 80,12</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>Agregar al Carrito</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/06/D13-8970-006/D13-8970-006_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Campiones muy buenos</strong>
+        <span>USD 80,12</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>Agregar al Carrito</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
